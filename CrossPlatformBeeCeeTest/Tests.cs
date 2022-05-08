cafcaff61c9e487e0a3911b55c1a79bb3fc7d341
Bill crowe <billcrowe22@gmail.com>
2022-05-08 01:53:01 +0100

    committing files for new user interface test

---
+++ CrossPlatformBeeCeeTest/Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace BeeCeeAutoFramework
+{
+    public class Tests
+    {
+        private IWebDriver Driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Console.WriteLine("Setup");
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+
+            Console.WriteLine("Test1");
+            Assert.Pass();
+        }
+    }
+}
